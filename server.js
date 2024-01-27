86968af378550f54e81d38e87c7b20ae608d13df
Talha Imran <talhaimran284@gmail.com>
2024-01-28 02:31:27 +0500

    Setup socket redirections

--- server.js
+++ server.js
@@ -4,12 +4,31 @@ import { Server } from 'socket.io'
 
 const app = express()
 const httpServer = createServer(app)
-const io = new Server(httpServer, {})
+const io = new Server(httpServer, {
+    cors: {
+        origin: '*',
+    },
+})
 
 io.on('connection', (socket) => {
-	console.log(`A user connected with socket ID: ${socket.id}`)
+    console.log(`A new user connected with socket ID: ${socket.id}`)
+
+    socket.on('offer', (offer) => {
+        console.log('Offer received on server: ', offer)
+        socket.broadcast.emit('offer', offer)
+    })
+
+    socket.on('answer', (answer) => {
+        console.log('Answer received on server: ', answer)
+        socket.broadcast.emit('answer', answer)
+    })
+
+    socket.on('ice-candidate', (iceCandidate) => {
+        console.log('Ice candidate received on server: ', iceCandidate)
+        socket.broadcast.emit('ice-candidate', iceCandidate)
+    })
 })
 
 httpServer.listen(process.env.PORT || 8000, () => {
-	console.log('Server is running')
+    console.log('Server is running')
 })
