8e42a063169d0811bdb9ef8af8a17b5a59efe831
Talha Imran <talhaimran284@gmail.com>
2024-02-04 20:56:33 +0500

    feat(add): added functionality to join & leave rooms

--- server.js
+++ server.js
@@ -10,22 +10,68 @@ const io = new Server(httpServer, {
     },
 })
 
+const roomUsers = new Map()
+
 io.on('connection', (socket) => {
     console.log(`A new user connected with socket ID: ${socket.id}`)
 
-    socket.on('offer', (offer) => {
-        console.log('Offer received on server: ', offer)
-        socket.broadcast.emit('offer', offer)
+    socket.on('offer', (offer, targetSocketId) => {
+        console.log(
+            `Offer received on server from ${socket.id} to be able to send to ${targetSocketId}`,
+            offer
+        )
+
+        io.to(targetSocketId).emit('offer', offer, socket.id)
     })
 
-    socket.on('answer', (answer) => {
-        console.log('Answer received on server: ', answer)
-        socket.broadcast.emit('answer', answer)
+    socket.on('answer', (answer, targetSocketId) => {
+        console.log(
+            `Answer received on server to be able to send to ${targetSocketId}`,
+            answer
+        )
+
+        io.to(targetSocketId).emit('answer', answer, socket.id)
     })
 
-    socket.on('ice-candidate', (iceCandidate) => {
-        console.log('Ice candidate received on server: ', iceCandidate)
-        socket.broadcast.emit('ice-candidate', iceCandidate)
+    socket.on('ice-candidate', (iceCandidate, targetSocketId) => {
+        console.log(`Ice candidate received on server to be able to send to ${targetSocketId} `, iceCandidate)
+
+        io.to(targetSocketId).emit('ice-candidate', iceCandidate, socket.id)
+    })
+
+    socket.on('create-meet-link', () => {
+        console.log('Create meet link received on server')
+
+        // TODO: replace this with uuid library to get a unique meet link
+        const meetLink = `${socket.id}-${new Date().getTime()}`
+
+        io.to(socket.id).emit('meet-link-created', meetLink)
+    })
+
+    socket.on('join-meet-link', (meetLink) => {
+        console.log('Join meet link received on server: ', meetLink)
+
+        socket.join(meetLink)
+
+        const users = roomUsers.get(meetLink) || []
+        users.push(socket.id)
+        roomUsers.set(meetLink, [...new Set(users)])
+
+        socket.to(meetLink).emit('user-joined', socket.id)
+    })
+
+    socket.on('disconnect', () => {
+        roomUsers.forEach((users, meetLink) => {
+            const index = users.indexOf(socket.id)
+
+            if (index > -1) {
+                users.splice(index, 1)
+                roomUsers.set(meetLink, users)
+                socket.to(meetLink).emit('user-left', socket.id)
+            }
+        })
+
+        console.log('After leave', roomUsers)
     })
 })
 
